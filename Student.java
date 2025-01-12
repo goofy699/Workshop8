bcd5a4e4eae50d997da3f6910721d0e12069a6ba
Prashanna karki <Prashannakarki@gmail.com>
2025-01-12 14:20:21 +0545

    Added getter methods to Student class

---
+++ Student.java
@@ -0,0 +1,47 @@
+public class Student {
+    private String name;
+    private int age;
+    private double[] grades;
+
+    // Constructor
+    public Student(String name, int age, double[] grades) {
+        this.name = name;
+        this.age = age;
+        this.grades = grades;
+    }
+
+    // Getter methods
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public double[] getGrades() {
+        return grades;
+    }
+
+    // Method to calculate average grade
+    public double calculateAverageGrade() {
+        double total = 0;
+        for (double grade : grades) {
+            total += grade;
+        }
+        return total / grades.length;
+    }
+
+    // Method to display student information
+    public void displayStudentInfo() {
+        System.out.println("Student Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.print("Grades: ");
+        for (double grade : grades) {
+            System.out.print(grade + " ");
+        }
+        System.out.println();
+        System.out.println("Average Grade: " + calculateAverageGrade());
+        System.out.println();
+    }
+}
